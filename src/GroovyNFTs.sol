685cdffa0995d1b36564995ddf35d9946c8f5481
e <abrandecarlo@protonmail.com>
2022-04-06 07:58:08 -0700

    now we can add to attributes

--- src/GroovyNFTs.sol
+++ src/GroovyNFTs.sol
@@ -14,6 +14,7 @@ contract GroovyNFTs is ERC721, Ownable {
     using Strings for uint256;
     string public baseURI;
     uint256 public currentTokenId;
+    // Max supply can be 4,294,967,295 if using 8 attributes
     uint256 public constant TOTAL_SUPPLY = 10_000;
     uint256 public constant MINT_PRICE = 0.08 ether;
 
@@ -32,13 +33,94 @@ contract GroovyNFTs is ERC721, Ownable {
 
 
     function mintTo(address recipient) public payable returns (uint256) {
-        // preload warm slot
-        currentTokenId;
         uint256 newTokenId = ++currentTokenId;
+
         if (newTokenId > TOTAL_SUPPLY) {
             revert MaxSupply();
         }
 
+        // Already set to zero
+        uint256 attributes_ = attributes[newTokenId];
+
+        // Load Attributes
+        uint32 a0;
+        uint32 a1;
+        uint32 a2;
+        uint32 a3;
+        uint32 a4;
+        uint32 a5;
+        uint32 a6;
+        uint32 a7;
+
+        // Do something with da numbas
+
+        // OR the uint32s to the attribute, starting with attribute a7
+        // Pretty much mash together the uint32s into a single uint256
+        assembly {
+            attributes_ := or(
+                    attributes_,
+                    a7
+            )
+
+            attributes_ := shr(
+                32,
+                or(
+                    attributes_,
+                    a6
+                )
+            )
+
+            attributes_ := shr(
+                64,
+                or(
+                    attributes_,
+                    a5
+                )
+            )
+
+            attributes_ := shr(
+                96,
+                or(
+                    attributes_,
+                    a4
+                )
+            )
+
+            attributes_ := shr(
+                128,
+                or(
+                    attributes_,
+                    a3
+                )
+            )
+
+            attributes_ := shr(
+                160,
+                or(
+                    attributes_,
+                    a2
+                )
+            )
+
+            attributes_ := shr(
+                196,
+                or(
+                    attributes_,
+                    a1
+                )
+            )
+
+            attributes_ := shr(
+                224,
+                or(
+                    attributes_,
+                    a0
+                )
+            )
+          
+        }
+
+        attributes[newTokenId] = attributes_;
         // cut attributes[tokenId] into 8 pieces and iterate each piece by 1
         // Once we got this down we can randomize the value (VRF, will be difficult to "throw away" values)
         _safeMint(recipient, newTokenId);
